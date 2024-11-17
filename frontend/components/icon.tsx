84a462a596f9843d8b49ff3c486afdd749a98f28
RakibulBh <rakibulbhuiyan1973@gmail.com>
2024-11-17 12:19:47 +0000

    add frontend layout fixes

---
+++ frontend/components/icon.tsx
@@ -0,0 +1,12 @@
+import { Laugh } from "lucide-react";
+import React from "react";
+
+const Icon = () => {
+  return (
+    <div>
+      <Laugh />
+    </div>
+  );
+};
+
+export default Icon;
--- frontend/components/sidebar.tsx
+++ frontend/components/sidebar.tsx
@@ -1,7 +1,10 @@
+import { Icon, Laugh } from "lucide-react";
 import React from "react";
 
 const Sidebar = () => {
-  return <div className="h-full w-32 bg-[#26272B] py-8 px-6"></div>;
+  return (
+    <div className="h-full w-20 bg-[#26272B] py-8 px-6 flex flex-col justify-between text-white"></div>
+  );
 };
 
 export default Sidebar;
